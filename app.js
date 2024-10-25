53be40cc47fd172e5618f21e6f6e1ed87d222e02
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-10-26 01:44:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const http = require("http")
+const path = require("path")
+const socketio = require("socket.io")
+
+const app = express()
+const server = http.createServer(app)
+
+const io = socketio(server)
+
+app.set("view engine", "ejs")
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, "public")))
+
+io.on("connection", (socket) => {
+    //console.log("connected io")
+    
+    socket.on("send-location", (data) => {
+        io.emit("receive-location", { id: socket.id, ...data })
+    })
+
+    socket.on("disconnect",()=>{
+        io.emit("user-disconnected",socket.id)
+    })
+})
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+server.listen(3000, () => {
+    console.log("Server is running")
+})
